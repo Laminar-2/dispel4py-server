eb206c88e22e7a5dac97e882fedac0ec6d548c28
Samuel Evans <sjevans400@gmail.com>
2023-10-19 15:23:29 +0100

    Fix PEController path bug

--- src/main/java/com/dispel4py/rest/controller/PEController.java
+++ src/main/java/com/dispel4py/rest/controller/PEController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/registry/{user}/pe/")
+@RequestMapping(path = "/registry/{user}/pe")
 public class PEController {
 
     private final PEService peService;

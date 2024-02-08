87a3c4b77183301ba6e2e511f27318fe541e2ecc
Sam Evans <sje7@st-andrews.ac.uk>
2024-02-08 12:11:15 +0000

    Revert "Revert "Change Flux type to Response""
    This reverts commit 4b9eb82af612dbb2337086aae5593875307129fd.

--- src/main/java/com/dispel4py/rest/service/ExecutionService.java
+++ src/main/java/com/dispel4py/rest/service/ExecutionService.java
@@ -1,9 +1,10 @@
 package com.dispel4py.rest.service;
 
 import com.dispel4py.rest.model.Execution;
+import com.dispel4py.rest.model.Response;
 import reactor.core.publisher.Flux;
 
 public interface ExecutionService {
 
-    Flux<String> runWorkflow(Execution e, String user);
+    Flux<Response> runWorkflow(Execution e, String user);
 }
